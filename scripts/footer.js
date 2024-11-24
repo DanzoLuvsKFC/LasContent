99fd0d7b58c49e7187565301409b73a3f5b81615
DanzoLuvsKFC <131270351+DanzoLuvsKFC@users.noreply.github.com>
2024-11-24 21:19:57 +0200

    YEEYEE YEEHHH

--- scripts/footer.js
+++ scripts/footer.js
@@ -6,21 +6,21 @@ window.onload = () => {
     footer.style.backgroundColor = "#022ddc";
     footer.style.color = "aliceblue";
 
-    // // Paragraph 1
-    // const contactInfo1 = document.createElement('p');
-    // contactInfo1.innerText = "ACADEMIC CONCEPT";
-    // contactInfo1.classList.add('contact-info-1');  // Custom class
-    // footer.appendChild(contactInfo1);
+    // Paragraph 1
+    const contactInfo1 = document.createElement('p');
+    contactInfo1.innerText = "Let's LasCollaborate together!";
+    contactInfo1.classList.add('contact-info-1');  // Custom class
+    footer.appendChild(contactInfo1);
 
-    // // Paragraph 2
-    // const contactInfo2 = document.createElement('p');
-    // contactInfo2.innerText = "This website was made for academic purposes only and only explores a new concept to present the original Lamborghini website.";
-    // contactInfo2.classList.add('contact-info-2');  // Custom class
-    // footer.appendChild(contactInfo2);
+    // Paragraph 2
+    const contactInfo2 = document.createElement('p');
+    contactInfo2.innerText = "Contact me: @daniellasco4@gmail.com";
+    contactInfo2.classList.add('contact-info-2');  // Custom class
+    footer.appendChild(contactInfo2);
 
     // Paragraph 3
     const contactInfo3 = document.createElement('p');
-    contactInfo3.innerText = "© 2024 | Designed by Danzo. All Rights Reserved.";
+    contactInfo3.innerText = "© 2024 | Designed by lasco. All Rights Reserved.";
     contactInfo3.classList.add('contact-info-3');  // Custom class
     footer.appendChild(contactInfo3);
 
